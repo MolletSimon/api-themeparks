dd4eee0b125419e85799f43aeaf0b969f7d3b568
Simon Mollet <mollet.simon.pro@gmail.com>
2020-05-07 05:06:43 +0200

    minor fixes

--- app.js
+++ app.js
@@ -2,8 +2,37 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
+const expressSwagger = require('express-swagger-generator')(app);
 
 const rideRoutes = require('./routes/rideWaitTimes');
+
+let options = {
+    swaggerDefinition: {
+        info: {
+            description: 'NodeJS API for Themeparks',
+            title: 'API Themeparks',
+            version: '1.0.0',
+        },
+        host: 'https://api-madera.cleverapps.io',
+        basePath: '/api',
+        produces: [
+            "application/json",
+            "application/xml"
+        ],
+        schemes: ['http'],
+        securityDefinitions: {
+            JWT: {
+                type: 'apiKey',
+                in: 'header',
+                name: 'Authorization',
+                description: "",
+            }
+        }
+    },
+    basedir: __dirname, //app absolute path
+    files: ['./routes/**/*.js'] //Path to the API handle folder
+};
+expressSwagger(options)
 
 mongoose.connect(
     'mongodb+srv://molletsimon:bspahOcz603eQvIT@cluster0-twfrq.mongodb.net/Themeparks?retryWrites=true&w=majority',
--- routes/rideWaitTimes.js
+++ routes/rideWaitTimes.js
@@ -2,7 +2,18 @@ const express = require('express');
 const router = express.Router();
 const rideCtrl = require('../controllers/rideTimes');
 
-router.get('wait-times/disney-park-paris', rideCtrl.getWaitTimesDisneylandParks);
-router.get('wait-times/disney-studio-paris', rideCtrl.getWaitTimesDisneylandStudio);
+/**
+ * @route GET /wait-times/disney-park-paris
+ * @group WaitTimes
+ * @return Ride Object
+ */
+router.get('/wait-times/disney-park-paris', rideCtrl.getWaitTimesDisneylandParks);
+
+/**
+ * @route GET /wait-times/disney-studio-paris
+ * @group WaitTimes
+ * @return Ride Object
+ */
+router.get('/wait-times/disney-studio-paris', rideCtrl.getWaitTimesDisneylandStudio);
 
 module.exports = router;
